1d68e79eab96e356bba6645faeff8078ec405720
Glory Ngum Foncha <gloriane7767@gmail.com>
2025-12-01 12:03:51 +0100

    Initial commit

---
+++ src/main/java/com/gloriane/Main.java
@@ -0,0 +1,11 @@
+package com.gloriane;
+
+public class Main {
+    public static String ex1 = "Java";
+    public static String ex2 = "Long example sentence";
+    
+    public static void main(String[] args) {
+        System.out.println(ex1.length());
+        System.out.println(ex2.charAt(6));
+    }
+}
---
+++ src/main/java/com/gloriane/StringDemo.java
@@ -0,0 +1,16 @@
+package com.gloriane;
+
+public class StringDemo {
+    public static String ex1 = "Java";
+
+    public static String ex2 ="Long example sentence";
+
+    public static String ex3 ="Even long example sentence";
+
+    public static void main(String[] args) {
+        System.out.println(ex1.length());
+        System.out.println(ex2.charAt(6));
+        System.out.println(ex3.indexOf("o"));
+    }
+}
+
