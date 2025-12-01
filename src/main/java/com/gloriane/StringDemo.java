62aad3765ffb3506278d6c36bef9546909942b35
Glory Ngum Foncha <gloriane7767@gmail.com>
2025-12-01 12:20:52 +0100

    update question 4 to 8

--- src/main/java/com/gloriane/StringDemo.java
+++ src/main/java/com/gloriane/StringDemo.java
@@ -7,10 +7,25 @@ public class StringDemo {
 
     public static String ex3 ="Even long example sentence";
 
+    public static String ex4 ="Ok this is not as long!";
+
+    public static String ex5 ="CAPS EQUALS SCREAMING";
+
+    public static String ex6 ="caps equals screaming";
+
+    public static String ex7 ="Java is the worst programming language!";
+
+    public static String ex8 ="\tj\ta\tv\ta\t";
+
     public static void main(String[] args) {
         System.out.println(ex1.length());
         System.out.println(ex2.charAt(6));
         System.out.println(ex3.indexOf("o"));
+        System.out.println(ex4.substring(0,22));
+        System.out.println(ex5.toLowerCase());
+        System.out.println(ex6.toUpperCase());
+        System.out.println(ex7.replace("worst", "best"));
+        System.out.println(ex8.trim());
     }
 }
 
